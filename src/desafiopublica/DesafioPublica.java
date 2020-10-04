be19b375b5b53391c53c1b81e579d1f9463a89f4
Matheus <matheusgpf@hotmail.com>
2020-10-04 19:06:07 -0300

    Atualizando documentação

--- src/desafiopublica/DesafioPublica.java
+++ src/desafiopublica/DesafioPublica.java
@@ -20,23 +20,23 @@ public class DesafioPublica  {
      int contmin;
      int contmax = 0;
            
-     Scanner sc = new Scanner(System.in);
+     Scanner sc = new Scanner(System.in); /* Permite que o sistema leia oque foi digitado pelo usuario*/
 
-     ArrayList<Tabela> list = new ArrayList<>();
+     ArrayList<Tabela> list = new ArrayList<>(); /*Instanciado uma lista para armazenar os elementos*/
     
-        Tabela   tabela0 = new Tabela(1,12,12,12,0,0);
+        Tabela   tabela0 = new Tabela(1,12,12,12,0,0); /* passando os valores da tabela criada por Maria atraves dos paramentros*/
         Tabela   tabela1 = new Tabela(2,24,12,24,0,1);
         Tabela   tabela2 = new Tabela(3,10,10,24,1,1);
         Tabela   tabela3 = new Tabela(4,24,10,24,1,1);
         
-        list.add(tabela0);
+        list.add(tabela0); /* essa função adiciona os objetos da tabela em uma lista*/
         list.add(tabela1);
         list.add(tabela2);
         list.add(tabela3);
         
        while (x!=0){ 
           
-       System. out.println("Digite 1 para inserir um novo jogo ");
+       System. out.println("Digite 1 para inserir um novo jogo "); /*menu onde o usuario ira escolher uma opcao para a tabela*/
        System. out.println("Digite 2 mostrar tabela");
        System. out.println("Digite 3 para remover um jogo");
        System. out.println("Digite 4 para ver a quantidade de recordes quebrados");
@@ -46,7 +46,7 @@ public class DesafioPublica  {
       
           switch (x){
             
-               case 1:
+               case 1: /* ira inserir uma nova tabela*/
                   
                   System.out.println("                                                           "); 
                   System.out.println("                                                           "); 
@@ -70,7 +70,7 @@ public class DesafioPublica  {
                   System.out.println("                                                           ");
                          break;
          
-                case 2: 
+                case 2: /* Imprime os objetos da tabela*/
                      
                   System.out.println("                                                           ");
                   System.out.println("                                                           ");
@@ -82,7 +82,7 @@ public class DesafioPublica  {
                  System.out.println("                                                           ");
                         break;
         
-                case 3:
+                case 3: /* Remove um jogo da tabela*/
          
                    System.out.println("-----------------------------------------------------------");
                    System.out.println("Digite um indice para remover da tabela, lembrando que começa em \"zero\"");
@@ -92,7 +92,7 @@ public class DesafioPublica  {
                    System.out.println("                                                           ");
                         break;
         
-                case 4:
+                case 4: /*Imprime a quantidade de recordes quebrados*/
                 
                 System.out.println("-----------------------------------------------------------");    
                 int b =0;   
